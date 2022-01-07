91ad6b407758b6f4a109c7455a3422b49666307f
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-07 09:11:04 -0300

    Seção 7 - Utilizando Strings de caractere na cláusula WHERE

--- Secao_7.sql
+++ Secao_7.sql
@@ -12,4 +12,16 @@ SELECT employee_id, last_name, job_id, department_id
 FROM employees
 WHERE job_id = 'IT_PROG';
 
+------------------------------------------------------
+-- Utilizando Strings de caractere na cláusula WHERE--
+------------------------------------------------------
+
+SELECT first_name, last_name, job_id, department_id, hire_date
+FROM employees
+WHERE last_name = 'King';
+
+SELECT first_name, last_name, job_id, department_id, hire_date
+FROM employees
+WHERE hire_date = '30/01/04';
+
 
