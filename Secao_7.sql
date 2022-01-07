e65591ac0c23b34f9b5fb2c750a401abdcbae92a
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-07 09:01:25 -0300

    Seção 7 - Utilizando a cláusula WHERE

---
+++ Secao_7.sql
@@ -0,0 +1,15 @@
+----------------------
+-- Seção 7 - Aula 1 --
+------------------------------------
+-- Restringindo e ordenando dados --
+------------------------------------
+
+--------------------------------
+-- Utilizando a cláusula WHERE--
+--------------------------------
+
+SELECT employee_id, last_name, job_id, department_id
+FROM employees
+WHERE job_id = 'IT_PROG';
+
+
