6217fff5ae06d15c973f348b8f109a8142620824
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-07 16:04:45 -0300

    Seção 8 - As cláusulas SQL são independentes

--- Secao_7.sql
+++ Secao_7.sql
@@ -227,31 +227,6 @@ DEFINE employee_id
 
 UNDEFINE employee_id
 
-----------------------
--- Seção 8 - Aula 1 --
-----------------------
-
------------------------------------
--- Utilizando funções single row --
------------------------------------
-
-------------------------------------------------
--- Funções de conversão maiúsculo e minusculo --
-------------------------------------------------
-
-SELECT employee_id, last_name,
-department_id
-FROM employees
-WHERE last_name = 'KING';
-
-SELECT employee_id, last_name,
-department_id
-FROM employees
-WHERE UPPER(last_name) = 'KING';
-
-
-
-
 
 
 
