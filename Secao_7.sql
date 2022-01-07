606b1f99253640cb3589b11ee38808175f23a3a3
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-07 09:23:53 -0300

    Seção 7 - Utilizando operadores de comparação na cláusula WHERE

--- Secao_7.sql
+++ Secao_7.sql
@@ -4,17 +4,17 @@
 -- Restringindo e ordenando dados --
 ------------------------------------
 
---------------------------------
--- Utilizando a cláusula WHERE--
---------------------------------
+---------------------------------
+-- Utilizando a cláusula WHERE --
+---------------------------------
 
 SELECT employee_id, last_name, job_id, department_id
 FROM employees
 WHERE job_id = 'IT_PROG';
 
-------------------------------------------------------
--- Utilizando Strings de caractere na cláusula WHERE--
-------------------------------------------------------
+-------------------------------------------------------
+-- Utilizando Strings de caractere na cláusula WHERE --
+-------------------------------------------------------
 
 SELECT first_name, last_name, job_id, department_id, hire_date
 FROM employees
@@ -23,5 +23,22 @@ WHERE last_name = 'King';
 SELECT first_name, last_name, job_id, department_id, hire_date
 FROM employees
 WHERE hire_date = '30/01/04';
+
+-----------------------------------------------------------
+-- Utilizando operadores de comparação na cláusula WHERE --
+-----------------------------------------------------------
+
+SELECT last_name, salary
+FROM employees
+WHERE salary >= 10000;
+
+
+
+
+
+
+
+
+
 
 
