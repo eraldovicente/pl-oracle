eb223c2a27043ac59b79a65a9e90acdc7bc2c445
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-07 19:31:00 -0300

    Seção 8 - Cálculos com datas

--- Secao_8.sql
+++ Secao_8.sql
@@ -86,7 +86,18 @@ FROM dual;
 SELECT 30000 * 1.25
 FROM dual;
 
-    
+------------------------
+-- Cálculos com datas --
+------------------------
+
+SELECT sysdate, sysdate + 30, sysdate + 60, sysdate - 30
+FROM dual;
+
+SELECT 
+    last_name, ROUND((SYSDATE - hire_date)/7, 2) "SEMANAS DE TRABALHO"
+FROM
+    employees;
+
     
     
     
