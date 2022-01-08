07b322b83b5c55250c01c44a26d43258e7acf79c
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-08 16:20:04 -0300

    Seção 9 - Utilizando a função TO_CHAR com números

---
+++ Secao_9.sql
@@ -0,0 +1,61 @@
+----------------------
+-- Seção 9 - Aula 1 --
+----------------------
+
+-------------------------------------------
+-- Utilizando a função TO_CHAR com datas --
+-------------------------------------------
+
+SELECT last_name, TO_CHAR(hire_date, 'DD/MM/YYYY HH24:MI:SS') DT_ADMISSÃO
+FROM employees;
+
+SELECT sysdate, TO_CHAR(sysdate, 'DD/MM/YYYY HH24:MI:SS') DATA
+FROM dual;
+
+SELECT last_name, TO_CHAR(hire_date, 'DD, "de" Month "de" YYYY') DT_ADMISSÃO
+FROM employees;
+
+SELECT last_name, TO_CHAR(hire_date, 'FMDD, "de" Month "de" YYYY') DT_ADMISSÃO
+FROM employees;
+
+---------------------------------------------
+-- Utilizando a função TO_CHAR com números --
+---------------------------------------------
+
+SELECT first_name, last_name, TO_CHAR(salary, 'L99G999G999D99') SALARY
+FROM employees;
+
+SELECT 
+    first_name, 
+    last_name, 
+    TO_CHAR(salary, 'FML99G999G999', 'NLS_CURRENCY=''R$ ''') SALARY
+FROM 
+    employees;
+
+SELECT 
+    first_name, 
+    last_name, 
+    TO_CHAR(0,'FML') || ' ' || 
+    TO_CHAR(salary, 'FML99G999G999') SALARY
+FROM 
+    employees;
+    
+ALTER SESSION SET nls_currency = 'R$';
+
+SELECT 
+    first_name, 
+    last_name, 
+    TO_CHAR(0,'FML') || ' ' || 
+    TO_CHAR(salary, 'FML99G999G999') SALARY
+FROM 
+    employees;
+
+    
+
+
+
+SELECT first_name, last_name, TO_CHAR(salary, 'L999D99') SALARY
+FROM employees;
+
+
+
