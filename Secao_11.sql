555fd76918435228e0ec0a862cd950cd30ff166f
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-09 18:39:39 -0300

    Seção 11 - aula 1 - Utilizando Alias de tabela

--- Secao_11.sql
+++ Secao_11.sql
@@ -10,28 +10,26 @@
 -- Utilizando prefixos - Colunas com nomes de tabelas --
 --------------------------------------------------------
 
-SELECT employees.employee_id, employees.last_name,
-       employees.department_id, departments.department_name
-FROM   employees JOIN departments
-       ON (employees.department_id = departments.department_id);
+SELECT
+    employees.employee_id,
+    employees.last_name,
+    employees.department_id,
+    departments.department_name
+FROM
+    employees
+    JOIN departments ON ( employees.department_id = departments.department_id );
 
+--------------------------------
+-- Utilizando Alias de tabela --
+--------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT
+    e.employee_id,
+    e.last_name,
+    e.department_id,
+    d.department_name
+FROM
+    employees e
+    JOIN departments d ON ( e.department_id = d.department_id );
+    
+    
