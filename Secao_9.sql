d3046c64f57dc708e845dfdbfe6ef4a169b3b6a3
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-08 23:00:48 -0300

    Seção 9 - Utilizando a função DECODE

--- Secao_9.sql
+++ Secao_9.sql
@@ -158,7 +158,16 @@ SELECT
                             END "NOVO SALÁRIO"
 FROM employees;
 
-                            
+--------------------------------
+-- Utilizando a função DECODE --
+--------------------------------
+
+SELECT last_name, job_id, salary,
+DECODE(job_id, 'IT_PROG' , (1.10 * salary),
+               'ST_CLERK', (1.15 * salary),
+               'SA_REP'  , (1.20 * salary)
+                         , salary) "NOVO SALÁRIO"
+FROM employees;    
    
    
    
