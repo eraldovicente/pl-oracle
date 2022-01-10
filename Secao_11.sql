ba2bce9c513a751d03f17f91461ca86a00fc6874
Eraldo Vicente <eraldovicente2@gmail.com>
2022-01-09 21:24:51 -0300

    sEÇÃO 11 - Incluindo condições adicionais a condição de JOIN utilizando AND

--- Secao_11.sql
+++ Secao_11.sql
@@ -117,3 +117,17 @@ FROM
 WHERE
     ( e.salary BETWEEN 10000 AND 15000 );
     
+----------------------------------------------------------------------
+-- Incluindo condições adicionais a condição de JOIN utilizando AND --
+----------------------------------------------------------------------
+
+SELECT
+    e.employee_id,
+    e.last_name,
+    e.salary,
+    e.department_id,
+    d.department_name
+FROM
+    employees e
+    JOIN departments d ON ( e.department_id = d.department_id )
+                          AND ( e.salary BETWEEN 10000 AND 15000 );
