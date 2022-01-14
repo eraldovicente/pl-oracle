6109a3ec053fecdebbe027e9a3368d0c5e519fe0
eraldovicente <eraldovicente2@gmail.com>
2022-01-14 17:41:59 -0300

    Oracle SQL - INNER joins

--- Secao_11.sql
+++ Secao_11.sql
@@ -42,12 +42,25 @@ SELECT
     location_id,
     city
 FROM
+    department
+    NATURAL INNER JOIN locations;
+
+SELECT
+    department_id,
+    department_name,
+    location_id,
+    city
+FROM
     departments
     NATURAL JOIN locations;
 
 -------------------------------
 -- JOIN com a cláusula USING --
 -------------------------------
+
+SELECT e.employee_id, e.last_name, d.location_id, department_id, d.department_name
+FROM employees e
+    INNER JOIN departments d USING (department_id);
 
 SELECT
     e.employee_id,
@@ -62,6 +75,21 @@ FROM
 --------------------------
 -- Join com cláusula ON --
 --------------------------
+
+SELECT
+    e.employee_id,
+    j.job_title,
+    d.department_name,
+    l.city,
+    l.state_province,
+    l.country_id
+FROM
+    employees e
+    INNER JOIN jobs        j ON e.job_id = j.job_id
+    INNER JOIN departments d ON d.department_id = e.department_id
+    INNER JOIN locations   l ON d.location_id = l.location_id
+ORDER BY
+    e.employee_id;
 
 SELECT
     e.employee_id,
