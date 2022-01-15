50cc8e7d00bf83129e872d67900be89e74759f6d
eraldovicente <eraldovicente2@gmail.com>
2022-01-15 20:11:43 -0300

    Joins entre várias tabelas utilizando sintaxe Oracle

--- Secao_11.sql
+++ Secao_11.sql
@@ -336,6 +336,20 @@ FROM employees e,
 WHERE (e.department_id = d.department_id)
 ORDER BY e.department_id;
 
+----------------------------------------------------------
+-- Joins entre várias tabelas utilizando sintaxe Oracle --
+----------------------------------------------------------
+
+SELECT e.employee_id, j.job_title, d.department_name, l.city, l.state_province, l.country_id
+FROM employees e,
+     jobs j,
+     departments d,
+     locations l
+WHERE (e.job_id = j.job_id) AND
+      (d.department_id = e.department_id) AND
+      (d.location_id = l.location_id)
+ORDER BY e.employee_id;
+     
 
 
 
