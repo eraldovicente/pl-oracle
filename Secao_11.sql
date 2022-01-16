82022ae0fab082af3c3ba99e98ba7e2d832b24e4
eraldovicente <eraldovicente2@gmail.com>
2022-01-16 16:40:15 -0300

    Seção 11 - aula 6 - Outer Join e Self Join utilizando sintaxe Oracle

--- Secao_11.sql
+++ Secao_11.sql
@@ -443,6 +443,17 @@ ORDER BY empregado.employee_id;
 
 DESC employees;
 
+------------------------------------------------------
+-- Outer Join e Self Join utilizando sintaxe Oracle --
+------------------------------------------------------
+
+SELECT empregado.employee_id "Id empregado", empregado.last_name "Sobrenome empregado",
+       gerente.employee_id "Id gerente", gerente.last_name "Sobrenome gerente"
+FROM employees empregado,
+     employees gerente
+WHERE (empregado.manager_id = gerente.employee_id(+))
+ORDER BY empregado.employee_id;
+
 
 
 
