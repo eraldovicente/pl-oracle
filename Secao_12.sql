6762a90efd7277e3e0bdd7a7100963ee3e1d3170
eraldovicente <eraldovicente2@gmail.com>
2022-01-16 19:23:17 -0300

    Seção 12 - aula 1 - Utilizando sub-consultas na cláusula HAVING

--- Secao_12.sql
+++ Secao_12.sql
@@ -15,6 +15,16 @@ FROM employees
 WHERE salary >
                 (SELECT AVG(NVL(salary,0))
                 FROM employees);
+
+-------------------------------------------------
+-- Utilizando sub-consultas na cláusula HAVING --
+-------------------------------------------------
+
+SELECT e1.department_id, MAX(e1.salary)
+FROM employees e1
+GROUP BY e1.department_id
+HAVING MAX(salary) < (SELECT AVG(e2.salary)
+                     FROM employees e2);
     
 
 
