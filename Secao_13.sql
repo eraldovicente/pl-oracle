1dd89b39c4310e114f5c9b1ca8873e839a9d0311
eraldovicente <eraldovicente2@gmail.com>
2022-01-17 13:36:50 -0300

    Seção 13 - aula 1 - Utilizando operador INTERSECT

--- Secao_13.sql
+++ Secao_13.sql
@@ -32,6 +32,22 @@ FROM employees
 WHERE department_id = 60
 ORDER BY employee_id;
 
+-----------------------------------
+-- Utilizando operador INTERSECT --
+-----------------------------------
+
+SELECT employee_id, job_id
+FROM employees
+WHERE job_id = 'IT_PROG'
+INTERSECT
+SELECT employee_id, job_id
+FROM employees
+WHERE department_id IN (60, 90, 100)
+ORDER BY employee_id;
+
+
+
+
 
 
 
