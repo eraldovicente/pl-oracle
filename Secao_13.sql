7b600ef474dea4cae0d0f60d3bd0b67d1d627c8e
eraldovicente <eraldovicente2@gmail.com>
2022-01-17 13:24:52 -0300

    Seção 13 - aula 1 - Utilizando o operador UNION ALL

--- Secao_13.sql
+++ Secao_13.sql
@@ -19,6 +19,18 @@ FROM employees
 WHERE job_id = 'IT_PROG'
 ORDER BY employee_id;
 
+-------------------------------------
+-- Utilizando o operador UNION ALL --
+-------------------------------------
+
+SELECT employee_id, job_id, hire_date, salary
+FROM employees
+WHERE job_id = 'IT_PROG'
+UNION ALL
+SELECT employee_id, job_id, hire_date, salary
+FROM employees
+WHERE department_id = 60
+ORDER BY employee_id;
 
 
 
