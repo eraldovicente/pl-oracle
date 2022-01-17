2c49831ba92ce9c1764436cfb395d171e454d431
eraldovicente <eraldovicente2@gmail.com>
2022-01-17 14:55:44 -0300

    Seção 14 - Inserindo linhas com valores nulos - método explícito

--- Secao_14.sql
+++ Secao_14.sql
@@ -14,6 +14,15 @@ INSERT INTO departments(department_id,
 department_name, manager_id, location_id)
 VALUES (280, 'Project Management', 103, 1400);
 
+-----------------------------------------------------------
+-- Inserindo linhas com valores nulos - método explícito --
+-----------------------------------------------------------
+
+desc departments
+
+INSERT INTO departments
+VALUES (290, 'Data Science', NULL, NULL);
+
 
 
 
