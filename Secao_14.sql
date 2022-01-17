f32ca7d702116a1e247a00023ba2352bff013773
eraldovicente <eraldovicente2@gmail.com>
2022-01-17 17:20:45 -0300

    Seção 14 - Insert utilizando data e hora especificas

--- Secao_14.sql
+++ Secao_14.sql
@@ -49,6 +49,24 @@ INSERT INTO employees
               '525.342.237', SYSDATE, 'IT_PROG',7000,
               NULL, 103, 60);
 
+-----------------------------------------------
+-- Insert utilizando data e hora especificas --
+-----------------------------------------------
+
+INSERT INTO employees
+            (employee_id, first_name, last_name, email,
+             phone_number, hire_date, job_id, salary,
+             commission_pct, manager_id, department_id)
+       VALUES (208, 'Vito', 'Corleone', 'VCORL',
+               '525.342.237', TO_DATE('11/02/2020', 'DD/MM/YYYY'), 'IT_PROG', 20000,
+               NULL, 103, 60);
+
+SELECT *
+FROM employees
+ORDER BY employee_id DESC;
+
+COMMIT;
+
 
 
 
