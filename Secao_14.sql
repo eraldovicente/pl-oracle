479dff434e4658a647a5a0d71059cf5dbe6e50f5
eraldovicente <eraldovicente2@gmail.com>
2022-01-17 14:39:49 -0300

    Seção 14 - Comandos DML - Utilizando o comando INSERT

---
+++ Secao_14.sql
@@ -0,0 +1,27 @@
+-----------------------
+-- Seção 14 - aula 1 --
+-----------------------
+
+--------------------------------------
+-- Comandos DML - Manipulando dados --
+--------------------------------------
+
+---------------------------------
+-- Utilizando o comando INSERT --
+---------------------------------
+
+INSERT INTO departments(department_id,
+department_name, manager_id, location_id)
+VALUES (280, 'Project Management', 103, 1400);
+
+
+
+
+
+
+
+
+
+
+
+
