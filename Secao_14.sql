89680a554fec61fe8278ba9b45f8f98455bbaca8
eraldovicente <eraldovicente2@gmail.com>
2022-01-17 18:09:34 -0300

    Seção 14 - Inserindo linhas a partir de uma sub-consulta

--- Secao_14.sql
+++ Secao_14.sql
@@ -81,5 +81,27 @@ ORDER BY department_id DESC;
 
 COMMIT;
 
+---------------------------------------------------
+-- Inserindo linhas a partir de uma sub-consulta --
+---------------------------------------------------
+
+DROP TABLE sales_reps;
+
+CREATE TABLE sales_reps
+(id NUMBER(6,0),
+ name VARCHAR(20),
+ salary NUMBER(8,2),
+ commission_pct NUMBER(2,2));
+
+INSERT INTO sales_reps(id, name, salary, commission_pct)
+SELECT employee_id, last_name, salary, commission_pct
+FROM employees
+WHERE job_id = 'SA_REP';
+
+COMMIT;
+
+SELECT *
+FROM sales_reps;
+
 
 
