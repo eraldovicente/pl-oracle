8334b1d0ad589e112794d8db1e165b20f85b9f5b
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 16:33:52 -0300

    Seção 16 - Definindo constraints  UNIQUE a nível de tabela

--- Secao_16.sql
+++ Secao_16.sql
@@ -76,9 +76,9 @@ DROP TABLE projects;
 
 CREATE TABLE projects (
     project_id    NUMBER(6) NOT NULL
-    CONSTRAINT projects_project_id_pk PRIMARY KEY,
+        CONSTRAINT projects_project_id_pk PRIMARY KEY,
     project_code  VARCHAR2(10) NOT NULL
-    CONSTRAINT projects_project_code_uk UNIQUE,
+        CONSTRAINT projects_project_code_uk UNIQUE,
     project_name  VARCHAR2(100) NOT NULL,
     department_id NUMBER(4) NOT NULL,
     creation_date DATE DEFAULT sysdate NOT NULL,
@@ -90,12 +90,24 @@ CREATE TABLE projects (
     description   VARCHAR2(400) NOT NULL
 );
 
+---------------------------------------------------------
+-- Definindo constraints UNIQUE a nível de tabela --
+---------------------------------------------------------
 
+DROP TABLE projects;
 
-
-
-
-
-
-
-
+CREATE TABLE projects (
+    project_id    NUMBER(6) NOT NULL,
+    project_code  VARCHAR2(10) NOT NULL,
+    project_name  VARCHAR2(100) NOT NULL,
+    department_id NUMBER(4) NOT NULL,
+    creation_date DATE DEFAULT sysdate NOT NULL,
+    start_date    DATE,
+    end_date      DATE,
+    status        VARCHAR2(20) NOT NULL,
+    priority      VARCHAR2(10) NOT NULL,
+    budget        NUMBER(11, 2) NOT NULL,
+    description   VARCHAR2(400) NOT NULL,
+    CONSTRAINT projects_project_id_pk PRIMARY KEY ( project_id ),
+    CONSTRAINT projects_project_code_uk UNIQUE ( project_code )
+);
