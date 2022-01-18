6054fc521421749e662a3b8c11df1593aed3b1b3
eraldovicente <eraldovicente2@gmail.com>
2022-01-17 21:11:53 -0300

    Seção 14 - Cláusula FOR UPDATE no comando SELECT

--- Secao_14.sql
+++ Secao_14.sql
@@ -189,8 +189,25 @@ SELECT *
 FROM employees
 ORDER BY employee_id DESC;
 
+-------------------------------------------
+-- Cláusula FOR UPDATE no comando SELECT --
+-------------------------------------------
 
+SELECT employee_id, salary, commission_pct, job_id
+FROM employees
+WHERE job_id = 'SA_REP'
+FOR UPDATE
+ORDER BY employee_id;
 
+COMMIT;
 
+SELECT e.employee_id, e.salary, e.commission_pct
+FROM employees e JOIN departments d
+USING (department_id)
+WHERE job_id = 'ST_CLERK'
+AND location_id = 1500
+FOR UPDATE of e.salary
+ORDER BY e.employee_id;
 
+COMMIT;
 
