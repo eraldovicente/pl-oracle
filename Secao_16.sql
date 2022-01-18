19c348a1c10aa9ed388a8fed7e0bdaa8947f957c
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 20:20:10 -0300

    Seção 16 - Violar constraints resulta em erro - CORREÇÃO

--- Secao_16.sql
+++ Secao_16.sql
@@ -311,3 +311,34 @@ INSERT INTO projects (
     1000000.00,
     'Oracle ERP Implementation'
 );
+
+
+---------------------------------------------------
+-- Violar constraints resulta em erro - CORREÇÃO --
+---------------------------------------------------
+
+INSERT INTO projects (
+    project_id,
+    project_code,
+    project_name,
+    department_id,
+    creation_date,
+    start_date,
+    end_date,
+    status,
+    priority,
+    budget,
+    description
+) VALUES (
+    1,
+    'ERPIMP',
+    'ERP Implementation',
+    60,
+    sysdate,
+    NULL,
+    NULL,
+    'Aproved',
+    'HIGH',
+    1000000.00,
+    'Oracle ERP Implementation'
+);
