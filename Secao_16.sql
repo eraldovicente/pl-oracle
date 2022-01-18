cec5402ef3035169bb5d044783f600f1ee27679e
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 17:06:36 -0300

    Seção 16 - Definindo constraints FOREIGN a nível de coluna

--- Secao_16.sql
+++ Secao_16.sql
@@ -113,7 +113,7 @@ CREATE TABLE projects (
 );
 
 -----------------------------------------------------
--- Definindo constraints FOREIGH a nível de coluna --
+-- Definindo constraints FOREIG a nível de coluna --
 -----------------------------------------------------
 
 DROP TABLE projects;
@@ -134,4 +134,28 @@ CREATE TABLE projects (
     priority      VARCHAR2(10) NOT NULL,
     budget        NUMBER(11, 2) NOT NULL,
     description   VARCHAR2(400) NOT NULL
+);
+
+-----------------------------------------------------
+-- Definindo constraints FOREIGN a nível de coluna --
+-----------------------------------------------------
+
+DROP TABLE projects;
+
+CREATE TABLE projects (
+    project_id    NUMBER(6) NOT NULL,
+    project_code  VARCHAR2(10) NOT NULL,
+    project_name  VARCHAR2(100) NOT NULL,
+    department_id NUMBER(4) NOT NULL,
+    creation_date DATE DEFAULT sysdate NOT NULL,
+    start_date    DATE,
+    end_date      DATE,
+    status        VARCHAR2(20) NOT NULL,
+    priority      VARCHAR2(10) NOT NULL,
+    budget        NUMBER(11, 2) NOT NULL,
+    description   VARCHAR2(400) NOT NULL,
+    CONSTRAINT projects_project_id_pk PRIMARY KEY ( project_id ),
+    CONSTRAINT projects_project_code_uk UNIQUE ( project_code ),
+    CONSTRAINT projects_department_id_fk FOREIGN KEY ( department_id )
+        REFERENCES departments ( department_id )
 );
