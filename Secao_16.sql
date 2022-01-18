aa2c29683b1b7333538609cd75c725a1467257e9
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 16:48:02 -0300

    Seção 16 - Definindo constraints FOREIGH a nível de coluna

--- Secao_16.sql
+++ Secao_16.sql
@@ -111,3 +111,27 @@ CREATE TABLE projects (
     CONSTRAINT projects_project_id_pk PRIMARY KEY ( project_id ),
     CONSTRAINT projects_project_code_uk UNIQUE ( project_code )
 );
+
+-----------------------------------------------------
+-- Definindo constraints FOREIGH a nível de coluna --
+-----------------------------------------------------
+
+DROP TABLE projects;
+
+CREATE TABLE projects (
+    project_id    NUMBER(6) NOT NULL
+        CONSTRAINT projects_project_id_pk PRIMARY KEY,
+    project_code  VARCHAR2(10) NOT NULL
+        CONSTRAINT projects_project_code_uk UNIQUE,
+    project_name  VARCHAR2(100) NOT NULL,
+    department_id NUMBER(4) NOT NULL
+        CONSTRAINT projects_department_id_fk
+            REFERENCES departments,
+    creation_date DATE DEFAULT sysdate NOT NULL,
+    start_date    DATE,
+    end_date      DATE,
+    status        VARCHAR2(20) NOT NULL,
+    priority      VARCHAR2(10) NOT NULL,
+    budget        NUMBER(11, 2) NOT NULL,
+    description   VARCHAR2(400) NOT NULL
+);
