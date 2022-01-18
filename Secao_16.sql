ae73263a97c27326e3f137a3922fea16950409c0
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 15:56:39 -0300

    Seção 16 - Definindo constraints PRIMARY KEY a nível de coluna

--- Secao_16.sql
+++ Secao_16.sql
@@ -12,8 +12,28 @@
 
 DROP TABLE projects;
 
-CREATE TABLE projects (
-    project_id    NUMBER(6) NOT NULL,
+CREATE TABLE projects 
+(   project_id    NUMBER(6) NOT NULL,
+    project_code  VARCHAR2(10) NOT NULL,
+    project_name  VARCHAR2(100) NOT NULL,
+    department_id NUMBER(4) NOT NULL,
+    creation_date DATE DEFAULT sysdate NOT NULL,
+    start_date    DATE,
+    end_date      DATE,
+    status        VARCHAR2(20) NOT NULL,
+    priority      VARCHAR2(10) NOT NULL,
+    budget        NUMBER(11, 2) NOT NULL,
+    description   VARCHAR2(400) NOT NULL);
+
+---------------------------------------------------------
+-- Definindo constraints PRIMARY KEY a nível de coluna --
+---------------------------------------------------------
+
+DROP TABLE projects;
+
+CREATE TABLE projects 
+(   project_id    NUMBER(6) NOT NULL
+    constraint    projects_project_id_pk PRIMARY KEY,
     project_code  VARCHAR2(10) NOT NULL,
     project_name  VARCHAR2(100) NOT NULL,
     department_id NUMBER(4) NOT NULL,
