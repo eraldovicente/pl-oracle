1bc5cdab70f20ec9d8cd3f76854d0ad33e7a0859
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 17:42:50 -0300

    Seção 16 - FOREIGN KEY - Regras de deleção -- Default no action

--- Secao_16.sql
+++ Secao_16.sql
@@ -159,3 +159,18 @@ CREATE TABLE projects (
     CONSTRAINT projects_department_id_fk FOREIGN KEY ( department_id )
         REFERENCES departments ( department_id )
 );
+
+----------------------------------------------------------
+-- FOREIGN KEY - Regras de deleção -- Default no action --
+----------------------------------------------------------
+
+DROP TABLE teams;
+
+CREATE TABLE teams (
+    project_id  NUMBER(6) NOT NULL,
+    employee_id NUMBER(6) NOT NULL,
+    CONSTRAINT teams_project_id_fk FOREIGN KEY ( project_id )
+        REFERENCES projects ( project_id ),
+    CONSTRAINT teams_employee_id_pk FOREIGN KEY ( employee_id )
+        REFERENCES employees ( employee_id )
+);
