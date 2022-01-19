b5d9ad331e031abf0ae315914e61cbadba1bbf0e
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 21:00:14 -0300

    Seção 16 - Consultando constraints pelo dicionário de dados

--- Secao_16.sql
+++ Secao_16.sql
@@ -342,3 +342,36 @@ INSERT INTO projects (
     1000000.00,
     'Oracle ERP Implementation'
 );
+
+----------------------------
+-- Efetivando a transação --
+----------------------------
+
+COMMIT;
+
+------------------------------------------------------
+-- Consultando constraints pelo dicionário de dados --
+------------------------------------------------------
+
+DESC user_constraints;
+
+DESC user_cons_columns;
+
+SELECT
+    co.constraint_name,
+    co.constraint_type,
+    co.search_condition,
+    co.r_constraint_name,
+    co.delete_rule,
+    cc.column_name,
+    cc.position,
+    co.status
+FROM
+    user_constraints co
+    JOIN user_cons_columns cc ON ( co.constraint_name = cc.constraint_name )
+                                 AND ( co.table_name = cc.table_name )
+WHERE
+    co.table_name = 'PROJECTS'
+ORDER BY
+    co.constraint_name,
+    cc.position;
