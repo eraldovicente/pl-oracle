a4ee25570ff15c8201f1dbf824aad8d5682de610
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 20:59:27 -0300

    Seção 25 - Utilizando variáveis BIND

--- Secao_25.sql
+++ Secao_25.sql
@@ -114,3 +114,46 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE('Numero Float = ' || vNumeroFloat);
   DBMS_OUTPUT.PUT_LINE('Numero Double = ' || vNumeroDouble);
 END;
+
+-------------------------------
+-- Utilizando variáveis BIND --
+-------------------------------
+
+SET SERVEROUTPUT ON
+VARIABLE gmedia NUMBER;
+DECLARE
+    vnumero1 NUMBER(11,2) := 2000;
+    vnumero2 NUMBER(11,2) := 5000;
+BEGIN
+    :gmedia := (vnumero1 + vnumero2) / 2;
+    DBMS_OUTPUT.PUT_LINE('Média = ' || TO_CHAR(:gmedia));
+EXCEPTION
+    WHEN OTHERS
+    THEN        
+        DBMS_OUTPUT.PUT_LINE('Erro Oracle: ' || SQLCODE || SQLERRM);
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
