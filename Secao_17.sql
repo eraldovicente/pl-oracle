212bbce65258b6cc43c08ba5f712ad2736a77779
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 00:09:26 -0300

    Seção 17 - Utilizando a cláusula CHECK OPTION

--- Secao_17.sql
+++ Secao_17.sql
@@ -39,6 +39,16 @@ GROUP BY e.department_id, department_name;
     
     SELECT *
     FROM vdepartments_total;
+    
+----------------------------------------
+-- Utilizando a cláusula CHECK OPTION --
+----------------------------------------
+
+CREATE OR REPLACE VIEW vemployeesdept100
+AS SELECT employee_id, first_name, last_name, department_id, salary
+FROM employees
+WHERE department_id = 100
+WITH CHECK OPTION CONSTRAINT vemployeesdept100_ck;
 
 
 
