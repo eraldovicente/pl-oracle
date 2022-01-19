1aaf538838eece544a63a65704adc80445717037
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 23:30:46 -0300

    Seção 17 - Criando uma visão

---
+++ Secao_17.sql
@@ -0,0 +1,51 @@
+-----------------------
+-- Seção 17 - aula 1 --
+-----------------------
+
+----------------------------------
+-- Criando e gerenciando visões --
+----------------------------------
+
+-----------------------
+-- Criando uma visão --
+-----------------------
+
+CREATE OR REPLACE VIEW vemployeesdept60
+AS SELECT employee_id, first_name, last_name, department_id, salary
+FROM employees
+WHERE department_id = 60;
+
+DESC vemployeesdept60;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
