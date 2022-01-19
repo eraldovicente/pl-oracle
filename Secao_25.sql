bd40e54c0c9e96a54400bedd6c8de0b239f8269b
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 16:29:59 -0300

    Seção 25 - Declarando variáveis tipo CHAR e VARCHAR2

--- Secao_25.sql
+++ Secao_25.sql
@@ -9,7 +9,17 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Número = ' || vNumero);
 END;
 
+-----------------------------------------------
+-- Declarando variáveis tipo CHAR e VARCHAR2 --
+-----------------------------------------------
 
+DECLARE 
+    vCaracterTamFixo CHAR(2) := 'RS';
+    vCaracterTamVariavel VARCHAR2(100) := 'Porto Alegre, RS';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('String caracteres tamanho fixo = ' || vCaracterTamFixo);
+    DBMS_OUTPUT.PUT_LINE('String caracteres tamanho variável = ' || vCaracterTamVariavel);
+END;
 
 
 
