09d88a6bcc88cd0137b2ab54a9efc20b8ed0485d
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 11:47:45 -0300

    Seção 19 - Analize o custo do comando no plano de execução

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pl-oracle
 Banco de Dados Oracle Completo: SQL+PLSQL+Modelagem de Dados - Emílio Scudero
+
+https://www.seas.upenn.edu/~zives/03f/cis550/codd.pdf
---
+++ Secao_19.sql
@@ -0,0 +1,48 @@
+-----------------------
+-- Seção 19 - aula 1 --
+-----------------------
+
+-----------------------------------------------------
+-- Analize o custo do comando no plano de execução --
+-----------------------------------------------------
+
+DROP INDEX emp_name_ix;
+DROP INDEX employees_last_name_idx;
+DROP INDEX employees_name_idx;
+
+SELECT *
+FROM employees
+WHERE last_name = 'Himuro';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
