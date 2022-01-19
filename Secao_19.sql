a495c457493ef9ed9a6110b95a8e692f23f8fa86
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 12:45:07 -0300

    Seção 19 - Consultando índices

--- Secao_19.sql
+++ Secao_19.sql
@@ -43,6 +43,22 @@ WHERE last_name = 'Himuro' AND
 
 ALTER INDEX employees_last_name_first_name_idx REBUILD; 
 
+-------------------------
+-- Consultando índices --
+-------------------------
+
+SELECT ix.index_name,
+       ic.column_position,
+       ic.column_name,
+       ix.index_type,
+       ix.uniqueness,
+       ix.status
+FROM user_indexes ix
+    JOIN user_ind_columns ic ON (ix.index_name = ic.index_name) AND
+                                (ix.table_name = ic.table_name)
+WHERE ix.table_name = 'EMPLOYEES'
+ORDER BY ix.index_name, ic.column_position;
+
 
 
 
