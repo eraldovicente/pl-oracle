aa7f391664485eab9319c002def5715e96d486bb
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 10:23:43 -0300

    Seção 18 - Utilizando uma sequência

--- Secao_18.sql
+++ Secao_18.sql
@@ -63,6 +63,27 @@ CREATE SEQUENCE employees_seq
  NOMAXVALUE
  NOCACHE
  NOCYCLE;
+ 
+ ------------------------------
+ -- Utilizando uma sequência --
+ ------------------------------
+ 
+ INSERT INTO employees
+            (employee_id, first_name, last_name, email,
+             phone_number, hire_date, job_id, salary,
+             commission_pct, manager_id, department_id)
+        VALUES (employees_seq.nextval, 'Paul', 'Simon', 'PSIMO',
+                '999.999.999', TO_DATE('12/02/2020', 'DD/MM/YYYY'), 'IT_PROG', 15000,
+                NULL, 103, 60);
+ 
+SELECT *
+FROM employees
+ORDER BY employee_id DESC;
+
+COMMIT;
+ 
+ 
+ 
 
 
 
