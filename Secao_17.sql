6bad5fc97950aabb31b1597e72d12ae1ddb1f80e
eraldovicente <eraldovicente2@gmail.com>
2022-01-18 23:59:12 -0300

    Seção 17 - Criando visões complexas

--- Secao_17.sql
+++ Secao_17.sql
@@ -11,7 +11,7 @@
 -----------------------
 
 CREATE OR REPLACE VIEW vemployeesdept60
-AS SELECT employee_id, first_name, last_name, department_id, salary
+AS SELECT employee_id, first_name, last_name, department_id, salary, commission_pct
 FROM employees
 WHERE department_id = 60;
 
@@ -23,6 +23,30 @@ DESC vemployeesdept60;
 
 SELECT *
 FROM vemployeesdept60;
+
+------------------------------
+-- Criando visões complexas --
+------------------------------
+
+CREATE OR REPLACE VIEW vdepartments_total
+(department_id, department_name, minsal, maxsal, avgsal)
+AS SELECT e.department_id, d.department_name, MIN(e.salary),
+    MAX(e.salary), AVG(e.salary)
+FROM employees e
+    JOIN departments d
+ON (e.department_id = d.department_id)
+GROUP BY e.department_id, department_name;
+    
+    SELECT *
+    FROM vdepartments_total;
+
+
+
+
+
+
+
+
 
 
 
