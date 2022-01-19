7cd91deb3280714c3d2876f73def040b03327126
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 13:48:45 -0300

    Seção 20 - Criando sinônimos privados

---
+++ Secao_20.sql
@@ -0,0 +1,56 @@
+-----------------------
+-- Seção 20 - aula 1 --
+-----------------------
+
+--------------------------------
+-- Criando sinônimos privados --
+--------------------------------
+
+CREATE SYNONYM departamentos
+FOR departments;
+ 
+CREATE SYNONYM dept
+FOR departments;
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
