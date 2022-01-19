b4daf4d73d6c75694251ae24d9ab08b24cd2e60b
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 15:51:46 -0300

    Seção 24 - Bloco Anônimo

---
+++ Secao_23.sql
@@ -0,0 +1,50 @@
+---------------------------------------------
+-- Seção 23 - Visão geral do Oracle PL/SQL --
+---------------------------------------------
+
+------------------------------
+-- Bloco anônimo  - exemplo --
+------------------------------
+
+SET SERVEROUTPUT  ON
+DECLARE 
+    vTexto VARCHAR2(100) := 'Seja bem vindo ao PL/SQL';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(vTexto);
+EXCEPTION
+    WHEN OTHERS
+    THEN
+        DBMS_OUTPUT.PUT_LINE('Erro Oracle: ' || SQLCODE || SQLERRM);
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
