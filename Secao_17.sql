8ad5d3c1419a343cb708e416fbf46c490fc3e7af
eraldovicente <eraldovicente2@gmail.com>
2022-01-19 00:14:56 -0300

    Seção 17 - Utilizando a cláusula READ ONLY

--- Secao_17.sql
+++ Secao_17.sql
@@ -50,6 +50,18 @@ FROM employees
 WHERE department_id = 100
 WITH CHECK OPTION CONSTRAINT vemployeesdept100_ck;
 
+-------------------------------------
+-- Utilizando a cláusula READ ONLY --
+-------------------------------------
+
+CREATE OR REPLACE VIEW vemployeesdept20
+AS SELECT employee_id, first_name, last_name, department_id, salary
+FROM employees
+WHERE department_id = 20
+WITH READ ONLY;
+
+
+
 
 
 
