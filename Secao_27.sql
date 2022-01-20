05b1299185457781dabf5c868cb5d9b7436df106
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 01:37:01 -0300

    Seção 27 - Erro ORA-01403 - No Data Found

--- Secao_27.sql
+++ Secao_27.sql
@@ -23,6 +23,26 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Salary: ' || vSalary);
 END;
 
+------------------------------------
+-- Erro ORA-01403 - No Data Found --
+------------------------------------
+
+SET SERVEROUTPUT ON
+DECLARE
+    vFirst_name employees.first_name%type;
+    vLast_name employees.last_name%type;
+    vSalary employees.salary%type;
+    vEmployee_id employees.employee_id%type := 50;
+BEGIN
+    SELECT first_name, last_name, salary
+    INTO vFirst_name, vLast_name, vSalary
+    FROM employees
+    WHERE employee_id = vEmployee_id;
+    DBMS_OUTPUT.PUT_LINE('Employee id: ' || vEmployee_id);
+    DBMS_OUTPUT.PUT_LINE('First name: ' || vFirst_name);
+    DBMS_OUTPUT.PUT_LINE('Last name: ' || vLast_name);
+    DBMS_OUTPUT.PUT_LINE('Salary: ' || vSalary);
+END;
 
 
 
