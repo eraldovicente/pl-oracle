759f9ac8b87e289903f3dc2ad695c9acc47c68aa
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 14:24:31 -0300

    Seção 28 - LOOP básico

--- Secao_27.sql
+++ Secao_27.sql
@@ -372,7 +372,23 @@ BEGIN
     --COMMIT;
 END;
 
+-----------------
+-- LOOP básico --
+-----------------
 
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT "Digite o valor do limite;
+DECLARE
+    vNumero NUMBER(38) := 1;
+    vLimite NUMBER(38) := &pLimite;
+BEGIN
+-- Imprimindo números de 1 até o limite
+LOOP
+   DBMS_OUTPUT.PUT_LINE('Número = ' || TO_CHAR(vNumero));
+   EXIT WHEN vNumero = vLimite;
+   vNumero := vNumero + 1;
+END LOOP;
+END;
 
 
 
