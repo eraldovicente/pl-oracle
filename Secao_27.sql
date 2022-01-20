af58689ad38824d7697a81a1c2c23bb4ee66def3
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 09:48:42 -0300

    Seção 27 - Utilizando o comando UPDATE no PL/SQL

--- Secao_27.sql
+++ Secao_27.sql
@@ -97,6 +97,41 @@ BEGIN
     COMMIT;
 END;
 
+-------------------------------------------
+-- Utilizando o comando UPDATE no PL/SQL --
+-------------------------------------------
+
+SET SERVEROUTPUT ON
+DECLARE
+    vEmployee_id employees.employee_id%type := 207;
+    vPercentual NUMBER(3) := 10;
+BEGIN
+    UPDATE employees
+    SET salary = salary * (1 + vPercentual / 100)
+    WHERE employee_id = vEmployee_id;
+    COMMIT;
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
