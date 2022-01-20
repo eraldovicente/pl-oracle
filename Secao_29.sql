34e342cdea2e4d8c937e37e0f225156cbfc5c12d
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 16:57:22 -0300

    Seção 29 - Criando um PL/SQL Record utilizando atributo %ROWTYPE

--- Secao_29.sql
+++ Secao_29.sql
@@ -45,3 +45,33 @@ BEGIN
                          || employee_record.phone_number);
 
 END;
+
+
+-----------------------------------------------------------
+-- Criando um PL/SQL Record utilizando atributo %ROWTYPE --
+-----------------------------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+ACCEPT pemployee_id PROMPT 'Digite o id do empregado: ';
+
+DECLARE
+    employee_record employees%rowtype;
+    vEmployee_id employees.employee_id%type := &pemployee_id;
+BEGIN
+    SELECT *
+    INTO employee_record
+    FROM
+        employees
+    WHERE
+        employee_id = vEmployee_id;
+
+    dbms_output.put_line(employee_record.employee_id
+                         || ' - '
+                         || employee_record.first_name
+                         || ' - '
+                         || employee_record.last_name
+                         || ' - '
+                         || employee_record.phone_number);
+
+END;
