f9ab8538f0d0efd7425cf2d567926671d00749f6
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 10:00:11 -0300

    Seção 27 - DELETE no PL/SQL

--- Secao_27.sql
+++ Secao_27.sql
@@ -112,6 +112,19 @@ BEGIN
     COMMIT;
 END;
 
+----------------------
+-- DELETE no PL/SQL --
+----------------------
+
+SET SERVEROUTPUT ON
+DECLARE
+    vEmployee_id employees.employee_id%type := 207;
+BEGIN
+    DELETE FROM employees
+    WHERE employee_id = vEmployee_id;
+    COMMIT;    
+END;
+
 
 
 
