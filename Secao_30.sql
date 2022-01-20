45f7295ee0071ed2bf7479a0c8472a9167920290
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 20:57:31 -0300

    Seção 30 - Nested Table of Records - Bulk Collect

--- Secao_30.sql
+++ Secao_30.sql
@@ -75,12 +75,12 @@ SET SERVEROUTPUT ON
 SET VERIFY OFF
 
 DECLARE
-    TYPE numero_table_type IS
-        TABLE OF NUMBER(2) INDEX BY BINARY_INTEGER;
+    TYPE numero_table_type IS TABLE OF NUMBER(2);
     numero_table numero_table_type := numero_table_type();
 BEGIN
     -- Armazena números de 1 a 10 em um Associative Array
     FOR i IN 1..10 LOOP
+        numero_table.extend;
         numero_table(i) := i;
     END LOOP;
 
@@ -89,6 +89,43 @@ BEGIN
                              || to_char(i)
                              || ', valor = '
                              || to_char(numero_table(i)));
+    END LOOP;
+
+END;
+
+--------------------------------------------
+-- Nested Table of Records - Bulk Collect --
+--------------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+DECLARE
+    TYPE employees_table_type IS TABLE OF employees%rowtype;
+    employees_table employees_table_type := employees_table_type();
+BEGIN
+    SELECT
+        *
+    BULK COLLECT
+    INTO employees_table
+    FROM
+        employees;
+
+    FOR i IN employees_table.first..employees_table.last LOOP
+        dbms_output.put_line(employees_table(i).employee_id
+                             || ' - '
+                             || employees_table(i).first_name
+                             || ' - '
+                             || employees_table(i).last_name
+                             || ' - '
+                             || employees_table(i).phone_number
+                             || ' - '
+                             || employees_table(i).job_id
+                             || ' - '
+                             || to_char(
+                                       employees_table(i).salary,
+                                       '99G999G999D99'
+                                ));
     END LOOP;
 
 END;
