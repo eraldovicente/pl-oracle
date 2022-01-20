d9ba4302c2f91f35fee5b90ec8613a023e1979ca
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 12:25:46 -0300

    Seção 28 - Utilizando o comando IF

--- Secao_27.sql
+++ Secao_27.sql
@@ -239,6 +239,48 @@ BEGIN
 END;
 
 
+-----------------------------
+-- Utilizando o comando IF --
+-----------------------------
+
+SET SERVEROUTPUT ON
+ACCEPT pdepartment_id PROMPT 'Digite o id do departamento';
+DECLARE
+    vpercentual NUMBER(3);
+    vDepartment_id employees.employee_id%type := &pdepartment_id;
+BEGIN
+    IF vDepartment_id = 80
+    THEN 
+        vpercentual := 10; --Sales
+    ELSE
+        IF vDepartment_id = 20
+        THEN
+            vpercentual := 15; --Marketing
+        ELSE
+            IF vDepartment_id = 60
+            THEN
+                vpercentual := 20; --IT
+            ELSE
+                vpercentual := 5;
+            END IF;
+        END IF;      
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('Id do departamento: ' || vDepartment_id);
+    DBMS_OUTPUT.PUT_LINE('Percentual: ' || vpercentual);
+    
+    UPDATE employees
+    SET salary = salary * (1 + vpercentual / 100)
+    WHERE department_id = vDepartment_id;
+    COMMIT;
+END;
+
+
+
+
+
+
+
 
 
 
