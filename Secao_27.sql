76c1625503b549c81ed67a29a5ad3cc53e59b417
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 09:28:49 -0300

    Seção 27 - Utilizando o comando INSERT do PL/SQL

--- Secao_27.sql
+++ Secao_27.sql
@@ -78,6 +78,24 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Somatório de salários: ' || vSum_Salary);
 END;
 
+-------------------------------------------
+-- Utilizando o comando INSERT do PL/SQL --
+-------------------------------------------
+
+SET SERVEROUTPUT ON
+DECLARE
+    vfirst_name employees.first_name%type;
+    vlast_name  employees.last_name%type;
+    vsalary     employees.salary%type;
+BEGIN
+    INSERT INTO employees
+    (employee_id, first_name, last_name, email, phone_number, hire_date, job_id,
+    salary, commission_pct, manager_id, department_id)
+    VALUES
+    (employees_seq.nextval, 'Kobe', 'Bryant', 'KBRYANT', '999.999.99999', SYSDATE,
+    'IT_PROG', 15000, 0.4, 103, 60);
+    COMMIT;
+END;
 
 
 
