d503525d4981567670045f0aa56efe1d291064e2
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 10:52:01 -0300

    Seção 27 - Savepoint

--- Secao_27.sql
+++ Secao_27.sql
@@ -162,6 +162,27 @@ END;
 --    COMMIT;
 --END;
 
+---------------
+-- Savepoint --
+---------------
+
+BEGIN
+    INSERT INTO employees
+    (employee_id, first_name, last_name, email, phone_number, hire_date, job_id,
+    salary, commission_pct, manager_id, department_id)
+    VALUES
+    (employees_seq.nextval, 'Kobe', 'Bryant', 'KBRYANT', '999.999.99999', SYSDATE,
+    'IT_PROG', 15000, 0.4, 103, 60);
+    
+    SAVEPOINT INSERTOK;
+    
+    UPDATE employees
+    SET salary = 30000
+    WHERE job_id = 'IT_PROG';
+    
+    ROLLBACK TO INSERTOK;
+    COMMIT;
+END;
 
 
 
