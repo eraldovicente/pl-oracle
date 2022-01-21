d0df5a0ef47b8c8a248750ee26cbce9ca8332ef5
eraldovicente <eraldovicente2@gmail.com>
2022-01-21 16:14:58 -0300

    Seção 33 - Métodos de passagem de parâmetro

--- Secao_33.sql
+++ Secao_33.sql
@@ -347,3 +347,51 @@ BEGIN
                                   )));
 
 END;
+
+--------------------------------------
+-- Métodos de passagem de parâmetro --
+--------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+DECLARE
+    employees_record employees%rowtype;
+BEGIN
+    prc_consulta_empregado(
+                          100,
+                          employees_record.first_name,
+                          employees_record.last_name,
+                          employees_record.email,
+                          employees_record.phone_number,
+                          employees_record.hire_date,
+                          employees_record.job_id,
+                          employees_record.salary,
+                          employees_record.commission_pct,
+                          employees_record.manager_id,
+                          employees_record.department_id
+    );
+
+    dbms_output.put_line(employees_record.first_name
+                         || ' '
+                         || employees_record.last_name
+                         || ' - '
+                         || employees_record.department_id
+                         || ' - '
+                         || employees_record.job_id
+                         || ' - '
+                         || employees_record.phone_number
+                         || ' - '
+                         || ltrim(to_char(
+                                         employees_record.salary,
+                                         'L99G999G999D99'
+                                  )));
+
+END;
+
+
+
+
+
+
+
