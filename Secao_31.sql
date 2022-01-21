1056e1094179601cd21397fa06f18baad5c3afe4
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 23:59:12 -0300

    Seção 31 - CURSOR FOR LOOP com parâmetros

--- Secao_31.sql
+++ Secao_31.sql
@@ -170,3 +170,47 @@ BEGIN
                                       )));
     END LOOP;
 END;
+
+------------------------------------
+-- CURSOR FOR LOOP com parâmetros --
+------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+DECLARE
+    CURSOR employees_cursor (
+        pdepartment_id NUMBER,
+        pjob_id        VARCHAR
+    ) IS
+    SELECT
+        *
+    FROM
+        employees
+    WHERE
+        department_id = pdepartment_id
+        AND job_id = pjob_id;
+
+BEGIN
+    FOR employees_record IN employees_cursor(
+                                            60,
+                                            'IT_PROG'
+                            ) LOOP
+        dbms_output.put_line(employees_record.employee_id
+                             || ' - '
+                             || employees_record.first_name
+                             || ' - '
+                             || employees_record.last_name
+                             || ' - '
+                             || employees_record.department_id
+                             || ' - '
+                             || employees_record.job_id
+                             || ' - '
+                             || employees_record.phone_number
+                             || ' - '
+                             || ltrim(to_char(
+                                             employees_record.salary,
+                                             'L99G999G999D99'
+                                      )));
+    END LOOP;
+END;
