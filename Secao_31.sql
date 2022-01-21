86cd193176db5d1e45a7c5953c794034cdb00116
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 23:23:03 -0300

    Seção 31 - Controlando um cursor explícito com WHILE LOOP

--- Secao_31.sql
+++ Secao_31.sql
@@ -12,72 +12,93 @@
 
 SET SERVEROUTPUT ON
 SET VERIFY OFF
+
 DECLARE
     CURSOR employees_cursor IS --
-    SELECT *                   ------ Declaração do Cursor  
-    FROM employees;            --
-    
+    SELECT
+        *                   ------ Declaração do Cursor  
+    FROM
+        employees;            --
+
     employees_record employees_cursor%rowtype;
 BEGIN
     /* Inicializa */
-    
+
     OPEN employees_cursor; -- Abrindo o cursor
     
     /* LOOP */
-    
+
     LOOP -- LOOP básico
-        FETCH employees_cursor
-        INTO employees_record; --Fetch do cursor
-        
+        FETCH employees_cursor INTO employees_record; --Fetch do cursor
+
         EXIT WHEN employees_cursor%notfound;
-        
         dbms_output.put_line(employees_record.employee_id
-                         || ' - '
-                         || employees_record.first_name
-                         || ' - '
-                         || employees_record.last_name
-                         || ' - '
-                         || employees_record.department_id
-                         || ' - '
-                         || employees_record.job_id
-                         || ' - '
-                         || employees_record.phone_number
-                         || ' - '
-                         || LTRIM(TO_CHAR(employees_record.salary, 'L99G999G999D99')));
+                             || ' - '
+                             || employees_record.first_name
+                             || ' - '
+                             || employees_record.last_name
+                             || ' - '
+                             || employees_record.department_id
+                             || ' - '
+                             || employees_record.job_id
+                             || ' - '
+                             || employees_record.phone_number
+                             || ' - '
+                             || ltrim(to_char(
+                                             employees_record.salary,
+                                             'L99G999G999D99'
+                                      )));
+
     END LOOP;
-    
+
     CLOSE employees_cursor; -- Fecha o cursor
 END;
 
+----------------------------------------------------
+-- Controlando um cursor explícito com WHILE LOOP --
+----------------------------------------------------
 
+SET SERVEROUTPUT ON
+SET VERIFY OFF
 
+DECLARE
+    CURSOR employees_cursor IS --
+    SELECT
+        *                   ------ Declaração do Cursor  
+    FROM
+        employees;            --
 
+    employees_record employees_cursor%rowtype;
+BEGIN
+    /* Inicializa */
 
+    OPEN employees_cursor; -- Abrindo o cursor
 
+    FETCH employees_cursor INTO employees_record; --Fetch do cursor
+    
+    /* LOOP */
 
+    WHILE employees_cursor%found LOOP
+        dbms_output.put_line(employees_record.employee_id
+                             || ' - '
+                             || employees_record.first_name
+                             || ' - '
+                             || employees_record.last_name
+                             || ' - '
+                             || employees_record.department_id
+                             || ' - '
+                             || employees_record.job_id
+                             || ' - '
+                             || employees_record.phone_number
+                             || ' - '
+                             || ltrim(to_char(
+                                             employees_record.salary,
+                                             'L99G999G999D99'
+                                      )));
 
+        FETCH employees_cursor INTO employees_record; --Fetch do cursor
 
+    END LOOP;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    CLOSE employees_cursor; -- Fecha o cursor
+END;
