5b21673c7ea31ba64a7acbc42e6a850ac9881c8a
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 23:11:13 -0300

    Seção 31 - Controlando um cursor explícito com LOOP básico

---
+++ Secao_31.sql
@@ -0,0 +1,83 @@
+--------------
+-- Seção 31 --
+--------------
+
+-------------------------------------
+-- Controlando um cursor explícito --
+-------------------------------------
+
+-----------------------------------------------------
+-- Controlando um cursor explícito com LOOP básico --
+-----------------------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    CURSOR employees_cursor IS --
+    SELECT *                   ------ Declaração do Cursor  
+    FROM employees;            --
+    
+    employees_record employees_cursor%rowtype;
+BEGIN
+    /* Inicializa */
+    
+    OPEN employees_cursor; -- Abrindo o cursor
+    
+    /* LOOP */
+    
+    LOOP -- LOOP básico
+        FETCH employees_cursor
+        INTO employees_record; --Fetch do cursor
+        
+        EXIT WHEN employees_cursor%notfound;
+        
+        dbms_output.put_line(employees_record.employee_id
+                         || ' - '
+                         || employees_record.first_name
+                         || ' - '
+                         || employees_record.last_name
+                         || ' - '
+                         || employees_record.department_id
+                         || ' - '
+                         || employees_record.job_id
+                         || ' - '
+                         || employees_record.phone_number
+                         || ' - '
+                         || LTRIM(TO_CHAR(employees_record.salary, 'L99G999G999D99')));
+    END LOOP;
+    
+    CLOSE employees_cursor; -- Fecha o cursor
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
