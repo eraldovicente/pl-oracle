d082571ee069c29f07eb9499fa3465d60cc579c1
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 23:46:03 -0300

    Seção 31 - CURSOR FOR LOOP utilizando sub-consulta

--- Secao_31.sql
+++ Secao_31.sql
@@ -111,14 +111,47 @@ SET SERVEROUTPUT ON
 SET VERIFY OFF
 
 DECLARE
-    CURSOR employees_cursor IS 
+    CURSOR employees_cursor IS
     SELECT
-        *                     
+        *
     FROM
         employees;
 
 BEGIN
     FOR employees_record IN employees_cursor LOOP
+        dbms_output.put_line(employees_record.employee_id
+                             || ' - '
+                             || employees_record.first_name
+                             || ' - '
+                             || employees_record.last_name
+                             || ' - '
+                             || employees_record.department_id
+                             || ' - '
+                             || employees_record.job_id
+                             || ' - '
+                             || employees_record.phone_number
+                             || ' - '
+                             || ltrim(to_char(
+                                             employees_record.salary,
+                                             'L99G999G999D99'
+                                      )));
+    END LOOP;
+END;
+
+---------------------------------------------
+-- CURSOR FOR LOOP utilizando sub-consulta --
+---------------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+BEGIN
+    FOR employees_record IN (
+        SELECT
+            *
+        FROM
+            employees
+    ) LOOP
         dbms_output.put_line(employees_record.employee_id
                              || ' - '
                              || employees_record.first_name
