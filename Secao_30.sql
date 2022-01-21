e3c28b13761e279b1939353fcdca755affa3c80a
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 21:28:01 -0300

    Seção 30 - Utilizando métodos para controlar collection

--- Secao_30.sql
+++ Secao_30.sql
@@ -192,3 +192,40 @@ BEGIN
 
 END;
 
+--------------------------------------------------
+-- Utilizando métodos para controlar collection --
+--------------------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+DECLARE
+    TYPE employees_table_type IS TABLE OF employees%rowtype;
+    employees_table employees_table_type := employees_table_type();
+BEGIN
+    SELECT
+        *
+    BULK COLLECT
+    INTO employees_table
+    FROM
+        employees;
+
+    FOR i IN employees_table.first..employees_table.last LOOP
+        dbms_output.put_line(employees_table(i).employee_id
+                             || ' - '
+                             || employees_table(i).first_name
+                             || ' - '
+                             || employees_table(i).last_name
+                             || ' - '
+                             || employees_table(i).phone_number
+                             || ' - '
+                             || employees_table(i).job_id
+                             || ' - '
+                             || to_char(
+                                       employees_table(i).salary,
+                                       '99G999G999D99'
+                                ));
+    END LOOP;
+
+END;
+
