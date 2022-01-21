5fb9e96c9b2801c81657eb3878b26cab3acb3afe
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 23:40:37 -0300

    Seção 31 - Controlando um Cursor explícito utilizando CURSOR FOR LOOP

--- Secao_31.sql
+++ Secao_31.sql
@@ -102,3 +102,38 @@ BEGIN
 
     CLOSE employees_cursor; -- Fecha o cursor
 END;
+
+----------------------------------------------------------------
+-- Controlando um Cursor explícito utilizando CURSOR FOR LOOP --
+----------------------------------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+DECLARE
+    CURSOR employees_cursor IS 
+    SELECT
+        *                     
+    FROM
+        employees;
+
+BEGIN
+    FOR employees_record IN employees_cursor LOOP
+        dbms_output.put_line(employees_record.employee_id
+                             || ' - '
+                             || employees_record.first_name
+                             || ' - '
+                             || employees_record.last_name
+                             || ' - '
+                             || employees_record.department_id
+                             || ' - '
+                             || employees_record.job_id
+                             || ' - '
+                             || employees_record.phone_number
+                             || ' - '
+                             || ltrim(to_char(
+                                             employees_record.salary,
+                                             'L99G999G999D99'
+                                      )));
+    END LOOP;
+END;
