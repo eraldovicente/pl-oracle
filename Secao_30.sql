0186c914c4338242cab1842de4ac978ba39e402f
eraldovicente <eraldovicente2@gmail.com>
2022-01-20 21:05:33 -0300

    Seção 30 - Varray

--- Secao_30.sql
+++ Secao_30.sql
@@ -66,7 +66,6 @@ BEGIN
 
 END;
 
-
 ------------------
 -- Nested Table --
 ------------------
@@ -129,3 +128,30 @@ BEGIN
     END LOOP;
 
 END;
+
+------------
+-- Varray --
+------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+DECLARE
+    TYPE numero_table_type IS VARRAY (10) OF INTEGER(2);
+    numero_table numero_table_type := numero_table_type();
+BEGIN
+    -- Armazena números de 1 a 10 em um Associative Array
+    FOR i IN 1..10 LOOP
+        numero_table.extend;
+        numero_table(i) := i;
+    END LOOP;
+
+    FOR i IN 1..10 LOOP
+        dbms_output.put_line('Associative Array: Indice = '
+                             || to_char(i)
+                             || ', valor = '
+                             || to_char(numero_table(i)));
+    END LOOP;
+
+END;
+
