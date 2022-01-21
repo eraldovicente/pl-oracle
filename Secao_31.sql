42b7a6f452036a1c1dde261d7ab91f0caacabce7
eraldovicente <eraldovicente2@gmail.com>
2022-01-21 00:50:21 -0300

    Seção 31 - Cursor explícito com SELECT FOR UPDATE

--- Secao_31.sql
+++ Secao_31.sql
@@ -214,3 +214,34 @@ BEGIN
                                       )));
     END LOOP;
 END;
+
+--------------------------------------------
+-- Cursor explícito com SELECT FOR UPDATE --
+--------------------------------------------
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+
+DECLARE
+    CURSOR employees_cursor (
+        pjob_id VARCHAR
+    ) IS
+    SELECT
+        *
+    FROM
+        employees
+    WHERE
+        job_id = pjob_id
+    FOR UPDATE;
+
+BEGIN
+    FOR employees_record IN employees_cursor('AD_VP') LOOP
+        UPDATE employees
+        SET
+            salary = salary * ( 1 + 10 / 100 )
+        WHERE
+            CURRENT OF employees_cursor;
+
+    END LOOP;
+    COMMIT;
+END;
