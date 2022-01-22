7cfbc553ae9af5afef76cd6352f61c2960100cf3
eraldovicente <eraldovicente2@gmail.com>
2022-01-22 19:05:37 -0300

    Seção 39 - Testando a validação de trigger

--- Secao_39.sql
+++ Secao_39.sql
@@ -6,19 +6,34 @@
 -- Database DML triggers a nível de comando --
 ----------------------------------------------
 
-CREATE OR REPLACE TRIGGER b_i_employees_s_trg BEFORE
-    INSERT ON employees
+CREATE OR REPLACE TRIGGER B_I_EMPLOYEES_S_TRG
+BEFORE INSERT
+ON employees
 BEGIN
-    IF ( to_char(
-        sysdate, 'DAY'
-    ) IN ( 'SABADO', 'DOMINDO' ) OR to_number(
-        to_char(
-            sysdate, 'HH24'
-        )
-    ) NOT BETWEEN 8 AND 18 ) THEN
-        raise_application_error(
-                               -20001,
-                               'O cadastro de empregados só é permitido em dias de semana dentro do horário comercial'
-        );
-    END IF;
+  IF  (TO_CHAR(SYSDATE, 'DAY') IN ('SÁBADO', 'DOMINGO') OR
+       TO_NUMBER(TO_CHAR(SYSDATE,'HH24')) NOT BETWEEN 8 AND 18) 
+  THEN
+       RAISE_APPLICATION_ERROR( -20001,'O cadastramento de Empregados só é permitido em dias de semana dentro do horário comercial');
+  END IF;
 END;
+
+-------------------------------------
+-- Testando a validação de trigger --
+-------------------------------------
+
+BEGIN
+    PCK_EMPREGADOS.PRC_INSERI_EMPREGADO('Fabiana', 'Paes', 'BIANA', '999.999.9999', SYSDATE,'IT_PROG',25000,NULL,103,60);
+    COMMIT;
+END;
+
+
+
+
+
+
+
+
+
+
+
+
