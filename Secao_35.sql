40ef7fc0d28a5eebf2459e51ac45446d58a8ec1d
eraldovicente <eraldovicente2@gmail.com>
2022-01-21 21:05:03 -0300

    Seção 35 - Consultando objetos tipo procedure e function de um usuário

---
+++ Secao_35.sql
@@ -0,0 +1,75 @@
+--------------
+-- Seção 35 --
+--------------
+
+--------------------------------------
+-- Gerenciando procedures e funções --
+--------------------------------------
+
+-----------------------------------------------------------------
+-- Consultando objetos tipo procedure e function de um usuário --
+-----------------------------------------------------------------
+
+DESC USER_OBJECTS;
+
+SELECT object_name, object_type, last_ddl_time, timestamp, status
+FROM user_objects
+WHERE object_type IN ('PROCEDURE', 'FUNCTION');
+
+SELECT object_name, object_type, last_ddl_time, timestamp, status
+FROM all_objects
+WHERE object_type IN ('PROCEDURE', 'FUNCTION');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
