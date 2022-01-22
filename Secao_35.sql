9ae4cee230b2de0b363d20108b5bc656f33eacf6
eraldovicente <eraldovicente2@gmail.com>
2022-01-21 21:11:48 -0300

    Seção 35 - Consultando objetos inválidos do schema do seu usuário

--- Secao_35.sql
+++ Secao_35.sql
@@ -20,7 +20,16 @@ SELECT object_name, object_type, last_ddl_time, timestamp, status
 FROM all_objects
 WHERE object_type IN ('PROCEDURE', 'FUNCTION');
 
+------------------------------------------------------------
+-- Consultando objetos inválidos do schema do seu usuário --
+------------------------------------------------------------
 
+
+DESC USER_OBJECTS;
+
+SELECT object_name, object_type, last_ddl_time, timestamp, status
+FROM user_objects
+WHERE status = 'INVALID';
 
 
 
