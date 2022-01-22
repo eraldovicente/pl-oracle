82ab785635f5a65addfa20fa0eb229e9fe35fee6
eraldovicente <eraldovicente2@gmail.com>
2022-01-22 19:19:10 -0300

    Seção 39 - Testando a validação de trigger

--- Secao_39.sql
+++ Secao_39.sql
@@ -50,7 +50,14 @@ BEGIN
   END IF;
 END;
 
+-------------------------------------
+-- Testando a validação de trigger --
+-------------------------------------
 
+BEGIN
+    PCK_EMPREGADOS.PRC_INSERI_EMPREGADO('Gabriel', 'Santana', 'BIRO', '999.999.9999', SYSDATE,'IT_PROG',25000,NULL,103,60);
+    COMMIT;
+END;
 
 
 
