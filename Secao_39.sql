228238466003b0ad96cf76709c268fc42c69f48e
eraldovicente <eraldovicente2@gmail.com>
2022-01-22 20:31:45 -0300

    Seção 39 - Criando a tabela de log de auditoria para a tabela EMPLOYEES para a coluna SALARY

--- Secao_39.sql
+++ Secao_39.sql
@@ -59,6 +59,59 @@ BEGIN
     COMMIT;
 END;
 
+--------------------------------------------
+-- Database DML triggers a nível de linha --
+--------------------------------------------
+
+---------------------------------------------------------------------------------------
+-- Criando a tabela de log de auditoria para a tabela EMPLOYEES para a coluna SALARY --
+---------------------------------------------------------------------------------------
+
+CREATE TABLE employees_log
+(employees_log_id NUMBER(11) NOT NULL,
+ dt_log DATE DEFAULT SYSDATE NOT NULL,
+ usuario VARCHAR2(30),
+ evento CHAR(1) NOT NULL,
+ employee_id NUMBER(6) NOT NULL,
+ salary_old NUMBER(8,2),
+ salary_new NUMBER(8,2));
+
+ALTER TABLE employees_log
+ADD CONSTRAINT employees_log_pk PRIMARY KEY (employees_log_id);
+
+CREATE SEQUENCE employees_log_seq
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE
+NOMAXVALUE;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
