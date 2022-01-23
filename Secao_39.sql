7726621d24a454478f91864ce0273e087eb868a7
eraldovicente <eraldovicente2@gmail.com>
2022-01-23 00:02:02 -0300

    Seção 39 - Resolvendo o problema de Mutating tables

--- Secao_39.sql
+++ Secao_39.sql
@@ -275,7 +275,57 @@ UPDATE employees
 SET salary = 70000
 WHERE employee_id = 100;
 
+DROP TRIGGER A_I_EMPLOYEES_R_TRG;
 
+DROP TRIGGER B_U_VALIDATE_SALARY_EMPLOYEES_R_TRG;
+
+----------------------------------------------
+-- Resolvendo o problema de Mutating tables --
+----------------------------------------------
+
+CREATE OR REPLACE PACKAGE PCK_EMPLOYEES_DADOS 
+AS
+  TYPE max_salary_table_type IS TABLE OF NUMBER(10,2)
+  INDEX BY BINARY_INTEGER;
+  
+  gMaxSalary  max_salary_table_type;
+
+END PCK_EMPLOYEES_DADOS;
+
+CREATE OR REPLACE TRIGGER B_IU_VALIDATE_SALARY_EMPLOYEES_S_TRG
+BEFORE INSERT OR UPDATE OF salary
+ON  employees
+DECLARE
+  vMaxSalary  employees.salary%TYPE;
+BEGIN
+  SELECT MAX(salary)
+  INTO   PCK_EMPLOYEES_DADOS.gMaxSalary(1)
+  FROM   employees;
+END;
+
+CREATE OR REPLACE TRIGGER B_IU_VALIDATE_SALARY_EMPLOYEES_R_TRG
+BEFORE INSERT OR UPDATE OF salary
+ON  employees
+FOR EACH ROW
+DECLARE
+  vMaxSalary  employees.salary%TYPE;
+BEGIN
+  IF  :new.salary  > PCK_EMPLOYEES_DADOS.gMaxSalary(1) * 1.2
+  THEN  
+      RAISE_APPLICATION_ERROR(-20001, 'Novo salario não pode ser superior ao maior salario + 20%');
+  END IF;
+END;
+
+----------------------------------
+-- Testando Violação da Regra 2 --
+----------------------------------
+
+SET VERIFY OFF
+UPDATE employees
+SET salary = 30000
+WHERE employee_id = 100;
+
+COMMIT;
 
 
 
