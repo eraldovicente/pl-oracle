56769e277396e5dba2a9fc7485b38b2f6c65f4d3
eraldovicente <eraldovicente2@gmail.com>
2022-01-22 21:36:30 -0300

    Seção 39 - Cláusula WHEN

--- Secao_39.sql
+++ Secao_39.sql
@@ -145,6 +145,61 @@ FROM employees_log;
 
 COMMIT;
 
+-------------------
+-- Cláusula WHEN --
+-------------------
+
+DELETE FROM employees_log;
+
+CREATE OR REPLACE TRIGGER A_IUD_EMPLOYEES_R_TRG
+    AFTER INSERT OR UPDATE OF SALARY OR DELETE
+    ON EMPLOYEES
+    FOR EACH ROW
+    WHEN (new.job_id <> 'AD_PRES')
+DECLARE
+    vevento employees_log.evento%type;
+    vemployee_id employees_log.employee_id%type;
+BEGIN
+    CASE
+        WHEN INSERTING THEN
+            vevento := 'I';
+            vemployee_id := :new.employee_id;
+        WHEN UPDATING THEN
+            vevento := 'U';
+            vemployee_id := :new.employee_id;
+        ELSE
+            vevento := 'D';
+            vemployee_id := :old.employee_id;
+    END CASE;
+    INSERT INTO employees_log
+        (employees_log_id,
+         dt_log,
+         usuario,
+         evento,
+         employee_id,
+         salary_old,
+         salary_new)
+    VALUES
+        (employees_log_seq.nextval,
+         SYSDATE,
+         USER,
+         vevento,
+         vemployee_id,
+         :old.salary,
+         :new.salary);
+END A_IUD_EMPLOYEES_R_TRG;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
